285cef50460386a877dff0ebacbc15ec37013878
Cemberk <Cem.bozkus@amd.com>
2024-07-29 12:07:28 -0500

    skip failures (#39)
    * skip failures
    
    * navi31 skip
    
    * mi300 skips
    
    * conversational test backwards compatability
    
    * mi300 skips

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -85,6 +85,7 @@ from transformers.testing_utils import (
     require_torch_sdpa,
     slow,
     torch_device,
+    skipIfRocm
 )
 from transformers.utils import (
     CONFIG_NAME,
@@ -3126,6 +3127,7 @@ class ModelTesterMixin:
     @require_accelerate
     @mark.accelerate_tests
     @require_torch_multi_accelerator
+    @skipIfRocm
     def test_model_parallelism(self):
         config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
 
